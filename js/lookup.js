dbd6b2412e51a600d7a4e7d228caeb0e16a4109b
dlisbbau <vinitkr@bbau.ac.in>
2018-08-03 17:21:59 +0530

    header

--- js/lookup.js
+++ js/lookup.js
@@ -18,7 +18,7 @@ $(document).ready( function (){
 		$.ajax({
 			beforeSend: function() { $('#wait').show(); },
 			url: queryUrl,
-			headers: {'Content-Type':'application/json'},
+			headers: {'Origin', 'X-Requested-With'},
 			dataType: "jsonp",
 			jsonp : 'callback',
 			complete: function() {  $('#wait').hide(); },
